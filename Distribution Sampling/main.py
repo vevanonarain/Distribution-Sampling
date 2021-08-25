2e0ce3054a7897a72a552853f71b0bc2b16531c6
vevanonarain <60396096+vevanonarain@users.noreply.github.com>
2021-08-25 20:33:09 +0530

    Add files via upload

---
+++ Distribution Sampling/main.py
@@ -0,0 +1,50 @@
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
+import pandas as pd
+import statistics
+import random
+import csv
+
+df = pd.read_csv("data.csv")
+data = df["temp"].tolist()
+
+def random_set_of_mean(counter):
+    dataset = []
+    for i in range(0, counter):
+        random_index = random.randint(0, len(data)-1)
+        value = data[random_index]
+        dataset.append(value)
+    mean = statistics.mean(dataset)
+    return mean
+
+def show_fig(mean_list):
+    df = mean_list
+    mean = statistics.mean(df)
+    fig = ff.create_distplot([df], ["temp"], show_hist = False)
+    fig.add_trace(go.Scatter(x = [mean, mean], y = [0, 1], mode = "lines", name = "MEAN"))
+
+def setup():
+    mean_list = []
+    for i in range(0, 1000):
+        set_of_means =  random_set_of_mean(100)
+        mean_list.append(set_of_means)
+    show_fig(mean_list)
+    
+    mean = statistics.mean(mean_list)
+    print("Mean of sampling solution is:", mean)
+
+setup()
+
+population_mean = statistics.mean(data)
+print("Population mean is: ", population_mean)
+
+def standard_deviation():
+    mean_list = []
+    for i in range(0, 1000):
+        set_of_means = random_set_of_mean(100)
+        mean_list.append(set_of_means)
+    
+    std_deviation = statistics.stdev(mean_list)
+    print("Standard Deviation is: ", std_deviation)
+
+standard_deviation()
